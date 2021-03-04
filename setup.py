0f85c5b80be2eaa153a2b0cda3642f4a88b5d749
Simon Geisler <geisler@in.tum.de>
2021-03-04 09:46:39 +0100

    fix: first install scipy

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ def system(command: str):
     logging.info(output)
 
 
+system(f'pip install scipy')
 system(f'pip install --no-index torch-scatter -f https://pytorch-geometric.com/whl/torch-{torch_v}+{cuda_v}.html')
 system(f'pip install --no-index torch-sparse -f https://pytorch-geometric.com/whl/torch-{torch_v}+{cuda_v}.html')
 system(f'pip install --no-index torch-cluster -f https://pytorch-geometric.com/whl/torch-{torch_v}+{cuda_v}.html')
